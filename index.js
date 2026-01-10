4a0b354092ab722fb4deb4c53483101d57f501e6
Jimish Ravat <jimishravat2802@gmail.com>
2026-01-10 20:39:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const seedDefaultAdmin = async () => {
 // Routes
 // app.use('/api/auth', require('./routes/auth'));
 // app.use('/api/users', require('./routes/users'));
-
+app.set("trust proxy", 1);
 // Health check
 app.get("/health", (req, res) => {
   res.status(200).json({
